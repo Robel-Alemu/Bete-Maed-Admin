3f9f1ca68f5ae7475450c756cc69ee2f64209060
Robel <robelalemu2372@gmail.com>
2023-11-15 21:05:23 +0300

    sideBar

--- Bete-Maed-Admin/src/main.tsx
+++ Bete-Maed-Admin/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import SidebarWithHeader from "./components/sidebar/SideBar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      {/* <App /> */}
+      <SidebarWithHeader />
     </ChakraProvider>
   </React.StrictMode>
 );
